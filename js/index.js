be4165f27cb9c03e25c16450d932674b8023f97f
gandanyang <842605220@qq.com>
2018-02-19 20:52:59 +0800

    123

--- js/index.js
+++ js/index.js
@@ -1,14 +1,10 @@
 $(document).ready(function() {
-    $('.picIcon').onclick = function() {
+    $('.picIcon').on('click', clickpicIcon());
+
+    function picIcon() {
 
     }
-
-
-
-
-    $("#sliderNav .next").click(function() {
-        movenext();
-    });
+    $("#sliderNav .next").on('click', movenext());
 
     function movenext() {
         $(".photoContainer li:first-child").animate({
@@ -30,22 +26,6 @@ $(document).ready(function() {
 
 
 
-
-
-
-    }
-    //增加准备动作 使动画更加协调
-
-
-
-
-
-
-
-
-    $("#sliderNav .prev").click(function() {
-        moveprev();
-    });
 
     function moveprev() {
         var temp = $(".photoContainer li:last-child").clone();
