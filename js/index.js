5f1af8228401f16fce480d14930d96019947cf13
gandanyang <842605220@qq.com>
2018-02-23 10:29:55 +0800

    asd

--- js/index.js
+++ js/index.js
@@ -5,29 +5,39 @@ $(document).ready(function() {
 
     // }
     // $("#sliderNav .next").on('click', movenext());
-    $("#sliderNav .next").click = movenext();
-
-    function movenext() {
-        $(".photoContainer li:first-child").animate({
-            marginLeft: "-430px"
-        }, 1000, function() {
-            var temp = $(this).clone();
-            $(this).remove();
-            temp.css({
-                marginLeft: "0"
+    $(".sliderNav .next").click = function() {
+            console.log(123);
+            // $(this).addClass("active").siblings("span").removeClass("active");
+            $(".photoContainer li:first-child").animate({
+                marginLeft: "-430px"
+            }, 1000, function() {
+                var temp = $(this).clone();
+                $(this).remove();
+                temp.css({
+                    marginLeft: "0"
+                });
+                $(".photoContainer").append(temp);
+                // console.log(this);
+                // var index = (this.dataset.index + 1) % 4;
+                // console.log(index);
+                // $('.photoContainer li:eq(2)').css({
+                //     marginLeft: "-860px"
+                // })
             });
-            $(".photoContainer").append(temp);
-            // console.log(this);
-            // var index = (this.dataset.index + 1) % 4;
-            // console.log(index);
-            // $('.photoContainer li:eq(2)').css({
-            //     marginLeft: "-860px"
-            // })
-        });
-    }
-    $("#sliderNav .prev").click = moveprev();
-    //增加准备动作 使动画更加协调
-    function moveprev() {
+        }
+        //定时器，定时进行切换
+         
+        // $('.photoContainer').hover(function() {    
+        //     clearInterval(timer);    
+        // }, function() {        
+        //     timer = setInterval(function() {        
+        //         showPic(index);        
+        //         index++;        
+        //         if (index == len) { index = 0; }        
+        //     }, 3000); 
+        // }).trigger('mouseleave');
+
+    $(".sliderNav .prev").onclick = function() {
         console.log(456);
         var temp = $(".photoContainer li:last-child").clone();
         $(".photoContainer li:last-child").remove();
